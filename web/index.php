8e73fab5c62001647825ba1eb63bfb0af3a0e705
suzukiyuna19871008 <yuna09027713882@gmail.com>
2016-10-16 09:56:28 +0900

    bottom update

--- web/index.php
+++ web/index.php
@@ -1,33 +1,9 @@
 <?php
+error_log('fbdev callback!');
 
-require('../vendor/autoload.php');
+$access_token = "[ACCESS TOKEN]";
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use GuzzleHttp\Client;
-
-$app = new Silex\Application();
-
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => 'php://stderr',
-));
-
-$app->before(function (Request $request) use($bot) {
-    // TODO validation
-});
-
-$app->get('/callback', function (Request $request) use ($app) {
-    $response = "";
-    if ($request->query->get('hub_verify_token') === getenv('FACEBOOK_PAGE_VERIFY_TOKEN')) {
-        $response = $request->query->get('hub_challenge');
-    }
-
-    return $response;
-});
-
-$app->post('/callback', function (Request $request) use ($app) {
-    // Let's hack from here!
-    // メッセージ受信
+// メッセージ受信
 $json_string = file_get_contents('php://input');
 error_log($json_string);
 $json_object = json_decode($json_string);
@@ -42,18 +18,51 @@ $post = <<< EOM
         "attachment":{
             "type":"template",
             "payload":{
-                "template_type":"button",
-                "text":"What do you want to do next?",
-                "buttons":[
+                "template_type":"generic",
+                "elements":[
                     {
-                        "type":"web_url",
-                        "url":"https://messengerplatform.fb.com",
-                        "title":"Show Website"
+                        "title":"Classic White T-Shirt",
+                        "image_url":"[画像URL]",
+                        "subtitle":"Soft white cotton t-shirt is back in style",
+                        "buttons":[
+                            {
+                                "type":"web_url",
+                                "url":"https://messengerplatform.fb.com",
+                                "title":"View Item"
+                            },
+                            {
+                                "type":"web_url",
+                                "url":"https://developers.facebook.com/docs/messenger-platform",
+                                "title":"Buy Item"
+                            },
+                            {
+                                "type":"postback",
+                                "title":"Bookmark Item",
+                                "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"
+                            }                            
+                        ]
                     },
                     {
-                        "type":"postback",
-                        "title":"Start Chatting",
-                        "payload":"USER_DEFINED_PAYLOAD"
+                        "title":"Classic Grey T-Shirt",
+                        "image_url":"[画像URL]",
+                        "subtitle":"Soft gray cotton t-shirt is back in style",
+                        "buttons":[
+                            {
+                                "type":"web_url",
+                                "url":"https://messengerplatform.fb.com",
+                                "title":"View Item"
+                            },
+                            {
+                                "type":"web_url",
+                                "url":"https://developers.facebook.com/docs/messenger-platform",
+                                "title":"Buy Item"
+                            },
+                            {
+                                "type":"postback",
+                                "title":"Bookmark Item",
+                                "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM101"
+                            }                            
+                        ]
                     }
                 ]
             }
@@ -62,39 +71,20 @@ $post = <<< EOM
 }
 EOM;
 
-    
-    
-    
-    /*
-    $body = json_decode($request->getContent(), true);
-    $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
+// メッセージ送信
+api_send_request($access_token, $post);
 
-    foreach ($body['entry'] as $obj) {
-        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
+function api_send_request($access_token, $post) {
+    $url = "https://graph.facebook.com/v2.6/me/messages?access_token={$access_token}";
+    $headers = array(
+        "Content-Type: application/json"
+    );
 
-        foreach ($obj['messaging'] as $m) {
-            $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
-            $from = $m['sender']['id'];
-            $text = $m['message']['text'];
-
-            if ($text) {
-                $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
-                $json = [
-                    'recipient' => [
-                        'id' => $from,
-                    ],
-                    'message' => [
-                        'text' => sprintf('%sじゃないかも', $text),
- 
-                    ],
-                ];
-                $client->request('POST', $path, ['json' => $json]);*/
-            }
-        }
-
-    }
-
-    return 0;
-});
-
-$app->run();
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $output = curl_exec($curl);
+}
